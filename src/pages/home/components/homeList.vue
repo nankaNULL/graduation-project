00b60e4b90da8f55af70412b39e6480bddcf7cc8
nankaNULL <534129087@qq.com>
2019-04-06 15:45:23 +0800

    首页列表联调

--- src/pages/home/components/homeList.vue
+++ src/pages/home/components/homeList.vue
@@ -2,31 +2,36 @@
   <div class="home-list">
     <div class="list-select">
       <div class="title" @click="handleOptionShow">
-        <span>{{params.optionId}}</span>
-        <span>icon</span>
+        <span>{{optionList[params.optionId].name}}</span>
+        <!-- <span>icon</span> -->
+        <i class="mint-toast-icon mintui mintui-back icon-back"></i>
       </div>
       <ul class="option-list" v-show="optionShow">
-        <li v-for="(item, key) in optionList" :key="key" @click="handleSelect(item)" 
-          :style="params.optionId === item ? 'color: yellow' : null"> {{item}}</li>
+        <li v-for="(item, key) in optionList" :key="key" @click="handleSelect(item.id)" 
+          :style="params.optionId === item.id ? 'color: yellow' : null"> {{item.name}}</li>
       </ul>
     </div>
     <div
       v-infinite-scroll="loadMore"
       infinite-scroll-disabled="loading"
       infinite-scroll-distance="10">
-      <mt-cell v-for="(item,key) in list" :key="key" class="cell-item">
-        <div>
-          <img src="" alt="">
+      <div v-for="(item,key) in list" :key="key" class="cell-item" @click="handleTurnToPage(item)">
+        <div class="cell-item-l">
+          <img class="item-image" :src="item['image_path']" :alt="item.name">
         </div>
-        <div>
-          <h4>jeep </h4>
+        <div class="cell-item-r">
+          <h3>{{item.name}}</h3>
           <div class="clearfix">
-            <p style="float:left">月售<span>1423</span></p>
-            <p style="float:right"><span>30</span>分钟 <span>1.3</span>km</p> 
+            <p style="float:left">月售: <span>{{item[item.recommend+'_recent_order_num']}}</span></p>
+            <p style="float:right;">
+              <span>{{item[item.recommend+'_lead_time']}}</span>分钟 
+              <span>{{1000 > item.distance ? item.distance + 'm' : (item.distance/1000).toFixed(2) + 'km'}}</span>
+            </p> 
           </div>
-          <p>起送 &yen; <span>1</span> 配送 &yen; <span>1</span> 人均 &yen; <span>11</span></p>
+          <p>起送 &yen; <span>{{item[item.recommend+'_order_amount']}}</span> 配送 &yen; <span>{{item[item.recommend+'_delivery_fee']}}</span> </p>
+          <p>推荐渠道：<span>{{item.recommend === 'elm' ? '饿了么' : '美团'}}</span> </p>
         </div>
-      </mt-cell>
+      </div>
       <p
         v-show="loading"
         class="page-infinite-loading"
@@ -39,6 +44,7 @@
 </template>
 <script>
 import Vue from 'vue';
+import { API } from "@/api/index.js";
 export default {
   name: "homeList",
   props: ['myType'],
@@ -46,11 +52,29 @@ export default {
     return {
       list: [],
       loading: false,
-      optionList: ['综合排序','配送速度','评分','起送价最低','配送费最低','人均由低到高'],
+      optionList: [{
+          name:'距离最近',
+          id:'0'
+        },{
+          name:'配送速度',
+          id:'1'
+        },{
+          name:'评分',
+          id:'2'
+        },{
+          name:'起送价最低',
+          id:'3'
+        },{
+          name:'配送费最低',
+          id:'4'
+        },{
+          name:'月售由高到低',
+          id:'5'
+        }],
       optionShow: false,
       
       params:{
-        optionId:'综合排序',
+        optionId: 0,
         type: this.myType,
       }
     };
@@ -59,19 +83,36 @@ export default {
     myType:function(type){
       this.params.type = type;
       this.list = [];
+      this.getShopList()
       this.loadMore();
     }
   },
+  mounted: function() {
+    this.getShopList(this.params.optionId);
+  },
   methods: {
+    // 获取列表
+    getShopList (sortId) {
+      API.getShopList({sortId}).then((result) => {
+        result.forEach(item => {
+          if (item['image_path'].slice(0,4) !== 'http' ){
+            let imgType = item['image_path'].slice(-3) == 'png' ? 'png' : 'jpeg';
+            item['image_path'] = 'https://fuss10.elemecdn.com/'+item['image_path'].slice(0,1)+'/'+item['image_path'].slice(1,3)+'/'+item['image_path'].slice(3)+'.'+imgType+'?imageMogr/format/webp/thumbnail/!130x130r/gravity/Center/crop/130x130/';
+          }
+        })
+        this.list = result;
+      })
+    },
     // 选择框的弹出
     handleOptionShow () {
       this.optionShow = !this.optionShow;
     },
     // 选择框的选择
-    handleSelect (option) {
-      this.params.optionId = option;
+    handleSelect (optionId) {
+      this.params.optionId = optionId;
       this.optionShow = false;
       this.list = [];
+      this.getShopList(optionId)
       this.loadMore();
     },
     // 列表加载
@@ -85,21 +126,51 @@ export default {
         this.loading = false;
       }, 2500);
     },
+    // 跳转到商品列表
+    handleTurnToPage(shop) {
+      const { elm_sid, mt_sid } = shop;
+      this.$router.push({ path:'/shop', query:{ id: elm_sid || mt_sid }})
+    }
   }
 };
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
   .home-list{
     border-top: 1px solid #e5e5e5;
     .list-select{
       .title, .option-list>li{
         padding: 15px 20px;
       }
+      .title{
+        font-size: 14px;
+        .icon-back{
+          display: inline-block;
+          transform: rotate(-90deg);
+        }
+      }
       .option-list{
         &>li{
           border-bottom: 1px solid #e5e5e5;
           &:last-child{border:0}
         }
+      }
+    }
+    
+    .cell-item{
+      padding: 10px;
+      overflow: hidden;
+      border-bottom : 1px solid #e5e5e5;
+      .cell-item-l, .cell-item-r{ float: left;}
+      .cell-item-l{
+        padding: 10px 20px 0 0;
+        .item-image{
+          display: inline-block;
+          height:130px;
+          width:130px;
+        }
+      }
+      .cell-item-r{
+        width: calc(100% - (150px));
       }
     }
   }
