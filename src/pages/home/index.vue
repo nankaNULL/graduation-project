165e3e64194ae505fa50780c506efcc6b91c9bd1
nankaNULL <534129087@qq.com>
2019-04-11 11:02:47 +0800

    分类图片添加

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -3,7 +3,7 @@
     <HomeSearch></HomeSearch>
     <div class="home-cell">
       <div v-for="(item, key) in cellData" :key="key" class="cell-item" @click="handleParamsChange(item.name)">
-        <img :src="item.src" alt="emm" width="24" height="24" >
+        <img :src="item.src" alt="emm" width="50" height="50" >
         <p>{{item.name}}</p>
       </div>
     </div>
@@ -24,16 +24,16 @@ export default {
   data() {
     return {
       cellData: [
-        { name: '简餐', src:'' },
-        { name: '包子粥店', src:'' },
-        { name: '川湘菜', src:'' },
-        { name: '汉堡薯条', src:'' },
-        { name: '盖浇饭', src:'' },
-        { name: '甜品', src:'' },
-        { name: '香锅砂锅', src:'' },
-        { name: '饺子馄饨', src:'' },
-        { name: '韩国炸鸡', src:'' },
-        { name: '米粉面馆', src:'' }
+        { name: '简餐', src:'https://fuss10.elemecdn.com/7/d8/a867c870b22bc74c87c348b75528djpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/' },
+        { name: '包子粥店', src:'https://fuss10.elemecdn.com/5/1a/dc885d2ce022d2ee60495acafb795jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/' },
+        { name: '川湘菜', src:'https://fuss10.elemecdn.com/a/8a/ec21096d528b7cfd23cdd894f01c6jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/' },
+        { name: '汉堡薯条', src:'https://fuss10.elemecdn.com/b/7f/432619fb21a40b05cd25d11eca02djpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/' },
+        { name: '盖浇饭', src:'https://fuss10.elemecdn.com/a/7b/b02bd836411c016935d258b300cfejpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/' },
+        { name: '甜品', src:'https://fuss10.elemecdn.com/7/d6/6f2631288a44ec177204e05cbcb93jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/' },
+        { name: '香锅砂锅', src:'https://fuss10.elemecdn.com/e/c7/b7ba9547aa700bd20d0420e1794a8jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/' },
+        { name: '饺子馄饨', src:'https://fuss10.elemecdn.com/0/1a/314b6da88ab6c7ae9828f91652d40jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/' },
+        { name: '韩国炸鸡', src:'https://fuss10.elemecdn.com/a/78/0fb469b2da210827ec16896e00420jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/' },
+        { name: '米粉面馆', src:'https://fuss10.elemecdn.com/e/89/185f7259ebda19e16123884a60ef2jpeg.jpeg?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/' }
       ],
       type: '简餐'
     };
@@ -50,8 +50,10 @@ export default {
   .home-cell{
     display: flex;
     flex-wrap: wrap;
+    background: white;
     .cell-item{
       width:20%;
+      text-align: center;
     }
     .mint-cell-wrapper{
       flex-wrap: wrap;
